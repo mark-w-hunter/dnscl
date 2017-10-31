b24c77797d4bd5dc89aeecf4fc37e503a47479c9
mhunter <mhunter@mmco11.local>
2017-10-30 21:44:06 -0400

    Update dnscl_domain function, add License

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# dnscl
+dnscl - This program analyzes DNS queries
+----
+
+dnscl_domain(...)
+    Returns a list of cllent IP addresses that queried a domain name
+    
+dnscl_ipaddress(...)
+    Returns a list of domain names queried by a client IP address
+    
+menu(...)
+    Prints main menu
+
+usage
+----
+
+1. Set syslog path/filename
+
+2. Set syslog field number for domain name
+
+3. Set syslog field number for IP address
+
+4. Run ./dnscl.py or python3 dnscl.py
+
--- dnscl.py
+++ dnscl.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
+
 # Copyright (c) 2017 Mark W. Hunter <marcus.w.hunter@gmail.com>
+# Version: 0.21
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact:
+#
+# Free Software Foundation           Voice:  +1-617-542-5942
+# 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652
+# Boston, MA  02110-1301,  USA       gnu@gnu.org
 
 """ This program analyzes DNS queries from syslog input """
 from itertools import groupby
@@ -40,27 +59,37 @@ def dnscl_ipaddress(ip_address):
 def dnscl_domain(domain_name):
     """ Returns cllent IP addresses that queried a domain name """
     my_list = []
+    my_domain_list = []
     line_count = 0
 
     for line in open(FILENAME):
         if domain_name in line:
             if "query:" in line:
                 fields = (line.strip().split(" "))
-                if len(fields) > 12:
+                if domain_name in fields[9] and len(fields) > 12:
                     ip_address = fields[6].split("#")  # field containing ip
                     my_list.append(ip_address[0])
+                    if domain_name != "":
+                        my_domain_list.append(fields[9])
                     line_count = line_count + 1
 
     my_set = sorted(set(my_list))
+    my_domain_set = sorted(set(my_domain_list))
     my_dict = dict([(a, len(list(b))) for a, b in groupby(sorted(my_list))])
     my_dict_view = [(v, k) for k, v in my_dict.items()]
     my_dict_view.sort(reverse=True)
 
     print(domain_name, "total queries are", line_count)
-    print("addresses: ")
+    print("ip addresses: ")
 
     for query_count, ip_address in my_dict_view:
         print(query_count, "\t", ip_address)
+   
+    if domain_name != "":
+        print("\ndomain names: ")
+
+        for domain_names_found in my_domain_set:
+            print(domain_names_found)
 
     print("\nSummary: Searched", domain_name, "and found", line_count,
           "queries from", len(my_set), "clients.")
